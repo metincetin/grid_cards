043c7c254ddc455c2b7871ea7c24cf54495f343d
Metin ÇETİN <metinc309@gmail.com>
2021-12-13 02:42:09 +0300

    Refactoring, added sprite system for cards

---
+++ modules/game_data.lua
@@ -0,0 +1,19 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+
+local M = {}
+
+function M.load_cards()
+	M.cards = json.decode(sys.load_resource("/data/cards.json"))
+end
+function M.get_card_by_name(value)
+	for i, card in ipairs(M.cards) do
+		if card.name == value then
+			return card
+		end
+	end
+end
+
+return M
